c66e229f835b12da1b907969b513f57b2a6b33af
Skandasoft <skandasoftinc@gmail.com>
2019-12-26 09:11:40 -0600

    clean version/unlink

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.helloWorld = (lang) => {
   if (lang === 'es') {
-    console.log('hola world in espanial linked up');
+    console.log('hola world in espanial ');
   } else {
-    console.log('hello world ver 1.0.3');
+    console.log('hello world ver 1.0.0');
   }
 };
